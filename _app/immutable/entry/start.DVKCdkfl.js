b7b735b503481a94fc444f98fb6d3df98d079a0d
UrbsKali <urbain.lantres@tutanota.com>
2026-04-07 01:00:22 +0200

    Updates

---
+++ _app/immutable/entry/start.DVKCdkfl.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/DQZ32maW.js";import{x as t}from"../chunks/Bf7RnJCB.js";export{t as load_css,r as start};
--- _app/immutable/entry/start.iVnetXXi.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/ZLG03_Je.js";import{x as t}from"../chunks/CJVWHjsJ.js";export{t as load_css,r as start};
